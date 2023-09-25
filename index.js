3d1b85e0dce4075ca20c16752ac51423e88e6041
vboxuser <khaoulaaourrath@gmail.com>
2023-09-25 14:29:42 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set();
+  for (const number of array) {
+    const complement = target - number;
+
+    if (seenNumbers.has(complement)) return true;
+    seenNumbers.add(number);
+  }
+  return false; 
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Time complexity: O(n)
+  Space complexity: O(n)
 */
 
 /* 
   Add your pseudocode here
+  - Create an empty Set called seenNumbers.
+  - Iterate over each number in the input array.
+  - For each number, calculate its complement by subtracting it from the target.
+  - Check if the complement is already in the seenNumbers Set.
+  - If the complement is found, return true (a pair that adds up to the target is found).
+  - Otherwise, add the current number to the seenNumbers Set.
+  - If no such pair is found after iterating through the entire array, return false.
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here:
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  // Add your own custom tests here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
